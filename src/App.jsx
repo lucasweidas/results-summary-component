61c472f1c57430862aa651e13740f074af672a19
Lucas Weidas <lucasweidas@gmail.com>
2023-03-12 15:14:06 -0300

    Add card component
    Add card data fetching and tags design
    
    Complete mobile version
    
    Complete desktop version
    
    Configure public and assets directories
    
    Change ErrorInfo component rendering location
    
    Add CardSkeleton component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from './components/Card.jsx';
+
+export default function App() {
+  return (
+    <>
+      <main className="md:flex md:min-h-screen md:justify-center md:py-8 md:items-center">
+        <Card />
+      </main>
+    </>
+  );
+}
